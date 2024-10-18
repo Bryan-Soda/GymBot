8ea646051ee25a2f23f4f5f1a8e87a43df064da1
Bryan-Soda <Bryansoto5623@gmail.com>
2024-10-17 18:46:55 -0700

    Update GymBot.py
    - Deleted unnecessary comments

--- GymBot.py
+++ GymBot.py
@@ -27,26 +27,5 @@ async def on_ready():
 @bot.command()
 async def test(ctx):
     await ctx.send("command evoked :3")
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-#     if message.guild is None:
-#         print("No guild :(")
-#         return
-
-#     print(f'Message from {message.author}: {message.content}')
-#     # brooklyn_99_quotes = [
-#     #     'I\'m the human form of the 💯 emoji.',
-#     #     'Bingpot!',
-#     #     (
-#     #         'Cool. Cool cool cool cool cool cool cool, '
-#     #         'no doubt no doubt no doubt no doubt.'
-#     #     ),
-#     # ]
-
-#     if message.content.strip() == 'ham':
-#         #response = random.choice(brooklyn_99_quotes)
-#         await message.channel.send("burger")
 
 bot.run(TOKEN)

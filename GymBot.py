df14a6c01356ea48a71f8fee5a88d5184b491756
Bryan-Soda <Bryansoto5623@gmail.com>
2024-10-17 19:39:39 -0700

    Update GymBot.py
    - Included other commands
    1. Bot sending an image/gif
    2. Bot sending a message formatted as an embed
    
    //Will need to create proper command functions for a leader board of sorts

--- GymBot.py
+++ GymBot.py
@@ -1,4 +1,4 @@
-# Gymbot.py
+#Gymbot.py
 import os
 import discord
 import random
@@ -24,8 +24,38 @@ async def on_ready():
                 f'{bot.user} is connected to the following guild:\n'
                 f'{guild.name}(id: {guild.id})'
             )
+        elif guild.name != GUILD:
+            print(f'{bot.user} is being dmed')
+
 @bot.command()
 async def test(ctx):
     await ctx.send("command evoked")
+@bot.command()
+async def moo(ctx):
+    await ctx.send("http://imgur.com/gallery/YiMUiop")     
+# @bot.command()
+# async def sendpr(ctx):
+#     await ctx.
 
+    #command template where the text is formatted as an embed. Will most likely use this format for most of the bots output
+@bot.command()
+async def embed(ctx):
+    embed=discord.Embed(
+    title="Text Formatting",
+        url="{url}",
+        description="Here are some ways to format text",
+        color=discord.Color.blue())
+    embed.set_author(name="Bot", url="{url}", icon_url="{urlimage}")
+    #embed.set_author(name=ctx.author.display_name, url="https://twitter.com/RealDrewData", icon_url=ctx.author.avatar_url)
+    embed.set_thumbnail(url="{urlimage}")
+    embed.add_field(name="*Italics*", value="Surround your text in asterisks ()", inline=False)
+    embed.add_field(name="**Bold**", value="Surround your text in double asterisks ()", inline=False)
+    embed.add_field(name="__Underline__", value="Surround your text in double underscores (\_\_)", inline=False)
+    embed.add_field(name="~~Strikethrough~~", value="Surround your text in double tildes (\~\~)", inline=False)
+    embed.add_field(name="`Code Chunks`", value="Surround your text in backticks (\`)", inline=False)
+    embed.add_field(name="Blockquotes", value="> Start your text with a greater than symbol (\>)", inline=False)
+    embed.add_field(name="Secrets", value="||Surround your text with double pipes (\|\|)||", inline=False)
+    embed.set_footer(text="Foot")
+    await ctx.send(embed=embed)
+#@bot.command()
 bot.run(TOKEN)
